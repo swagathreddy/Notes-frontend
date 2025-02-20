a6e29648268bf505a547087e9b7cf48b465944c3
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-20 15:55:20 +0530

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const API_URL = process.env.REACT_APP_API_URL  || "http://127.0.0.1:8000";
+const API_URL = process.env.REACT_APP_API_URL  || "http://127.0.0.1:8000" || "https://notes-frontend-eight-omega.vercel.app";
 const core=axios.create({
     baseURL: API_URL,
 })
