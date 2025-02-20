87884efdca1fd321b09b0a8e0fe5db353aa099ad
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-02-20 16:17:46 +0530

    Update core.js

--- src/core.js
+++ src/core.js
@@ -1,21 +1,23 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
-const API_URL = process.env.REACT_APP_API_URL  || "http://127.0.0.1:8000" || "https://notes-frontend-eight-omega.vercel.app";
-const core=axios.create({
+const API_URL = "https://notes-backend-production-5d4d.up.railway.app";  // Always use deployed backend
+
+const core = axios.create({
     baseURL: API_URL,
-})
+});
 
 core.interceptors.request.use(
     (config) => {
-        const token =localStorage.getItem(ACCESS_TOKEN);
-        if(token){
-            config.headers.Authorization=`Bearer ${token}`
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        return config;
     },
-    (error)=>{
-        return Promise.reject(error)
+    (error) => {
+        return Promise.reject(error);
     }
-)
-export default core
+);
+
+export default core;
